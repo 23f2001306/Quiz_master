6c202ccde6a7ea7db8742b2bd69a5df80f9cc429
23f2001306 <23f2001306@ds.study.iitm.ac.in>
2025-01-14 13:17:16 +0530

    Second commit

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, DateField
+from wtforms.validators import InputRequired, DataRequired, EqualTo, Email
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
     
 class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_pswd = PasswordField('Password', validators=[DataRequired(),EqualTo('password')])
+    confirm_pswd = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    name = StringField('Full Name', validators=[InputRequired()])
+    qualification = StringField('Qualification',validators=[InputRequired()])
+    dob =  DateField('Date of Birth',format='%d-%m-%Y', validators=[InputRequired()])
+    submit = SubmitField('Signup')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, SignupForm
 
 @app.route("/login")
 def login():
     form = LoginForm()
     return render_template("login.html",form = form)
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template("signup.html",form = form)
